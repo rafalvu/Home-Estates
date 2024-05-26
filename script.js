e6e0bf703b0fa2663ecc66af9175f561eb53bd15
rafalvu <rafal.walczewski00@gmail.com>
2024-05-26 20:03:49 +0200

    creating "rynek-pierwotny" page

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    speed: 500,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
