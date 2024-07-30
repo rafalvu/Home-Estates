4a42e339bb69b38b96dcf7f333cfc0b127c5d51d
rafalvu <rafal.walczewski00@gmail.com>
2024-07-30 10:56:29 +0200

    minor fixes in the navbar section

--- front/js/script-rynek-wtorny.js
+++ front/js/script-rynek-wtorny.js
@@ -1,3 +1,30 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function autoResize(textarea) {
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
+}
+
+// Select all textareas on the page
+var textareas = document.querySelectorAll("textarea");
+
+// Attach the autoResize function to the input event
+for (let textarea of textareas) {
+  textarea.addEventListener("input", function () {
+    autoResize(this);
+  });
+
+  // Call the function initially to resize on page load
+  autoResize(textarea);
+}
 const url = "https://homeestates.pl/api/proxy";
 
 fetch(url, {
